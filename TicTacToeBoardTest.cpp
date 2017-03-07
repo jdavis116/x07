5193f82f18c1061f78a9af8a7c9a8dd051f9beec
student <student@softeng>
2017-03-06 21:30:25 -0800

    Tested

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -24,10 +24,8 @@ TEST(TicTacToeBoardTest, toggleTurn)
   TicTacToeBoard board;
   board.placePiece(0,0);
   ASSERT_EQ(board.getPiece(0,0), X);
-  board.toggleTurn();
   board.placePiece(0,1);
   ASSERT_EQ(board.getPiece(0,1), O);
-  board.toggleTurn();
   board.placePiece(0,2);
   ASSERT_EQ(board.getPiece(0,2), X);
 }
@@ -35,5 +33,75 @@ TEST(TicTacToeBoardTest, toggleTurn)
 TEST(TicTacToeBoard, clearBoard)
 {
   TicTacToeBoard board;
-  for
+  for (int i = 0; i < BOARDSIZE; i++)
+  {
+    for (int j = 0; j < BOARDSIZE; j++)
+    {
+      ASSERT_EQ(board.getPiece(i,j), Blank);
+      board.placePiece(i,j);
+    }
+  }
+  board.clearBoard();
+  for (int i = 0; i < BOARDSIZE; i++)
+  {
+    for (int j = 0; j < BOARDSIZE; j++)
+    {
+      ASSERT_EQ(board.getPiece(i,j), Blank);
+    }
+  }
+}
+
+TEST(TicTacToeBoard, getWinner)
+{
+  TicTacToeBoard board;
+  board.placePiece(0,0);
+  board.placePiece(1,0);
+  board.placePiece(0,1);
+  board.placePiece(1,2);
+  board.placePiece(0,2);
+  ASSERT_EQ(board.getWinner(), X);
+  board.clearBoard();
+  board.placePiece(0,2);
+  board.placePiece(0,1);
+  board.placePiece(1,0);
+  board.placePiece(1,1);
+  board.placePiece(1,2);
+  board.placePiece(2,1);
+  ASSERT_EQ(board.getWinner(), O);
+  board.clearBoard();
+  board.placePiece(0,0);
+  board.placePiece(0,1);
+  board.placePiece(1,1);
+  board.placePiece(1,2);
+  board.placePiece(2,2);
+  ASSERT_EQ(board.getWinner(), X);
+  board.clearBoard();
+  board.placePiece(0,0);
+  board.placePiece(0,2);
+  board.placePiece(0,1);
+  board.placePiece(1,1);
+  board.placePiece(2,2);
+  board.placePiece(0,2);
+  ASSERT_EQ(board.getWinner(), O);
+  board.clearBoard();
+  ASSERT_EQ(board.getWinner(), Invalid);
+  board.placePiece(0,0);
+  ASSERT_EQ(board.getWinner(), Invalid);
+  board.placePiece(0,1);
+  ASSERT_EQ(board.getWinner(), Invalid);
+  board.placePiece(0,2);
+  ASSERT_EQ(board.getWinner(), Invalid);
+  board.placePiece(1,1);
+  ASSERT_EQ(board.getWinner(), Invalid);
+  board.placePiece(1,0);
+  ASSERT_EQ(board.getWinner(), Invalid);
+  board.placePiece(1,2);
+  ASSERT_EQ(board.getWinner(), Invalid);
+  board.placePiece(2,2);
+  ASSERT_EQ(board.getWinner(), Invalid);
+  board.placePiece(2,0);
+  ASSERT_EQ(board.getWinner(), Invalid);
+  board.placePiece(2,1);
+  ASSERT_EQ(board.getWinner(), Blank);
+  board.clearBoard();
 }
